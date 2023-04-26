9e96500b48feb6460053ac62a2f6039a4d69adf4
Maulana Fiqri Nurul Fawaid <maulanafiqri310@gmail.com>
2023-04-26 07:51:26 +0700

    fix

--- thr-km4-fe/remove-element-left-right/main.js
+++ thr-km4-fe/remove-element-left-right/main.js
@@ -1,8 +1,14 @@
 function removeLeftRight(arr, left, right) {
   if (left === "left"){
     arr.shift();
-    return arr
+  }else if (right !== "left"){
+    arr.pop();
+  }else if(right === "right"){
+    arr.pop();
+  } else if (left !== "right"){
+    arr.shift();
   }
+  return arr
 }
 console.log(removeLeftRight([1, 2, 3, 4, 5], "left")); // Output: [2, 3, 4, 5]
 console.log(removeLeftRight([1, 2, 3, 4, 5], "right")); // Output: [1, 2, 3, 4]
